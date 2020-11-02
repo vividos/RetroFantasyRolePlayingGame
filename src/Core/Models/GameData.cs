34207149e190a764a7782b19e996b9372c4ac385
Michael Fink <michael.fink@asamnet.de>
2020-11-02 21:54:20 +0100

    added a first draft of the GameData class, complete with serialization

---
+++ src/Core/Models/GameData.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace Game.Core.Models
+{
+    /// <summary>
+    /// The game's data
+    /// </summary>
+    public class GameData
+    {
+        /// <summary>
+        /// Game name
+        /// </summary>
+        public static string GameName { get; set; } = "Retro Fantasy RPG";
+
+        /// <summary>
+        /// Subtitle of the game name
+        /// </summary>
+        public static string GameSubtitle { get; set; } = "The Test Of Data";
+
+        /// <summary>
+        /// Creates a new game data instance
+        /// </summary>
+        /// <returns>game data instance</returns>
+        public static GameData Create()
+        {
+            return new GameData
+            {
+            };
+        }
+
+        /// <summary>
+        /// Saves game data to the stream
+        /// </summary>
+        /// <param name="stream">stream to save game to</param>
+        public void Save(Stream stream)
+        {
+            string json = JsonConvert.SerializeObject(this);
+
+            using (var writer = new StreamWriter(stream))
+            {
+                writer.Write(json);
+            }
+        }
+
+        /// <summary>
+        /// Loads game data from given stream
+        /// </summary>
+        /// <param name="stream">stream to load game data from</param>
+        /// <returns>loaded game data</returns>
+        public static GameData Load(Stream stream)
+        {
+            using (var reader = new StreamReader(stream))
+            {
+                string json = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<GameData>(json);
+            }
+        }
+    }
+}
