2c2f96d9c46bf5cf09344ce8e737aff038e75c2b
Michael Fink <michael.fink@asamnet.de>
2020-11-04 22:36:10 +0100

    added empty in-game screen and view model

---
+++ src/Core/ViewModels/IngameViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Game.Core.ViewModels
+{
+    /// <summary>
+    /// View model for the ingame screen.
+    /// </summary>
+    internal class IngameViewModel
+    {
+        /// <summary>
+        /// Interaction mode for the ingame view model
+        /// </summary>
+        internal enum InteractionMode
+        {
+            /// <summary>
+            /// Showing the game's screen
+            /// </summary>
+            Normal,
+
+            /// <summary>
+            /// Showing the inventory screen above the game's screen
+            /// </summary>
+            Inventory,
+
+            /// <summary>
+            /// Showing the stats screen above the game's screen
+            /// </summary>
+            Stats,
+        }
+    }
+}
---
+++ src/Core/Views/IngameScreen.cs
@@ -0,0 +1,52 @@
+﻿using Game.Core.ViewModels;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended.Screens;
+
+namespace Game.Core.Views
+{
+    /// <summary>
+    /// Game screen that shows the in-game views.
+    /// </summary>
+    internal class IngameScreen : GameScreen
+    {
+        /// <summary>
+        /// View model for the in-game screen
+        /// </summary>
+        private readonly IngameViewModel viewModel;
+
+        /// <summary>
+        /// Creates a new in-game screen object
+        /// </summary>
+        /// <param name="game">game object</param>
+        public IngameScreen(TheGame game)
+            : base(game)
+        {
+            this.viewModel = new IngameViewModel();
+        }
+
+        /// <summary>
+        /// Loads content for in-game screen
+        /// </summary>
+        public override void LoadContent()
+        {
+        }
+
+        /// <summary>
+        /// Updates screen state, e.g. from keyboard or gamepad state
+        /// </summary>
+        /// <param name="gameTime">game time; unused</param>
+        public override void Update(GameTime gameTime)
+        {
+            // TODO implement
+        }
+
+        /// <summary>
+        /// Draws the in-game screen
+        /// </summary>
+        /// <param name="gameTime">game time; unused</param>
+        public override void Draw(GameTime gameTime)
+        {
+            // TODO implement
+        }
+    }
+}
