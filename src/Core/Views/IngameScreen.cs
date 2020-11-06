a47b285e9a662a0ad0d2a69330080f0fc18c1cae
Michael Fink <michael.fink@asamnet.de>
2020-11-06 13:15:18 +0100

    added initial in-game screen layout, based on colored Boxes
    (to be replaced box by box)

--- src/Core/Views/IngameScreen.cs
+++ src/Core/Views/IngameScreen.cs
@@ -1,5 +1,7 @@
 ﻿using Game.Core.ViewModels;
 using Microsoft.Xna.Framework;
+using MonoGame.Extended.Gui;
+using MonoGame.Extended.Gui.Controls;
 using MonoGame.Extended.Screens;
 
 namespace Game.Core.Views
@@ -22,6 +24,89 @@ namespace Game.Core.Views
             : base(game)
         {
             this.viewModel = new IngameViewModel();
+
+            this.InitUserInterface(game);
+        }
+
+        /// <summary>
+        /// Initializes user interface
+        /// </summary>
+        /// <param name="game">game instance</param>
+        private void InitUserInterface(TheGame game)
+        {
+            var mapView = new Box
+            {
+                Width = 480,
+                Height = 480,
+                BorderThickness = 0,
+                FillColor = Color.LightBlue,
+            };
+
+            var partyListView = new Box
+            {
+                Height = 200,
+                Width = 320,
+                BorderThickness = 0,
+                FillColor = Color.DarkOrange,
+            };
+
+            var messageScrollView = new Box
+            {
+                Height = 200,
+                Width = 320,
+                BorderThickness = 0,
+                FillColor = Color.LawnGreen,
+            };
+
+            var controlButtonsGrid = new Box
+            {
+                Height = 80,
+                Width = 320,
+                BorderThickness = 0,
+                FillColor = Color.Pink,
+            };
+
+            var leftStackPanel = new StackPanel
+            {
+                Orientation = Orientation.Vertical,
+                Width = 480,
+                BorderThickness = 0,
+                BackgroundColor = Color.Red,
+                Items =
+                {
+                    mapView,
+                }
+            };
+
+            var rightStackPanel = new StackPanel
+            {
+                Orientation = Orientation.Vertical,
+                Width = 320,
+                BorderThickness = 0,
+                BackgroundColor = Color.Green,
+                Items =
+                {
+                    partyListView,
+                    messageScrollView,
+                    controlButtonsGrid,
+                }
+            };
+
+            // this stack panel splits the screen in two halves, left and right side
+            var content = new StackPanel
+            {
+                Orientation = Orientation.Horizontal,
+                Width = game.ScreenWidth,
+                Height = game.ScreenHeight,
+                Spacing = 0,
+                Items =
+                {
+                    leftStackPanel,
+                    rightStackPanel
+                }
+            };
+
+            game.SetGuiScreenContent(content);
         }
 
         /// <summary>
@@ -29,6 +114,7 @@ namespace Game.Core.Views
         /// </summary>
         public override void LoadContent()
         {
+            // TODO implement
         }
 
         /// <summary>
