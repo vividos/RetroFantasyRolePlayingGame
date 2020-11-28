8dcb01ce45fd270ef95c8da8c9d4841440dd0bc5
Michael Fink <michael.fink@asamnet.de>
2020-11-28 19:13:37 +0100

    added objects subset tileset from u4 tileset

---
+++ src/Content/tilemaps/objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="objects" tilewidth="32" tileheight="32" tilecount="176" columns="16">
+ <image source="objects.png" trans="000000" width="512" height="352"/>
+</tileset>
